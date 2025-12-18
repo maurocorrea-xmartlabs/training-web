f75b9ee2f05cead0809250d736c9c5c5acf9c4d7
Mauro Correa <mauro.correa@xmartlabs.com>
2025-12-18 11:40:14 -0300

    chore: project setup, still non-functional

---
+++ my-nextjs-app/my-next-app/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NextJs to-do list app",
+  description: "Created for the XmartLabs JS training",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SubjectsProvider>{children}</SubjectsProvider>
+      </body>
+    </html>
+  );
+}
---
+++ my-nextjs-app/my-next-app/contexts/subjectsProvider.ts
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import type { Subject } from "../types/subject";
+import {
+  getSubjects,
+  postSubject,
+  deleteSubject as deleteSubjectApi,
+} from "../controllers/subjectController";
+
+("use client");
+
+type SubjectsContextType = {
+  subjects: Subject[];
+  reloadSubjects: () => Promise<void>;
+  addSubject: (name: string, monthlyCost: number) => Promise<void>;
+  deleteSubject: (id: string) => Promise<void>;
+};
+
+const SubjectsContext = createContext<SubjectsContextType | null>(null);
+
+export function SubjectsProvider({ children }: { children: React.ReactNode }) {
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+
+  async function reloadSubjects() {
+    const data = await getSubjects();
+    if (data) setSubjects(data);
+  }
+
+  async function addSubject(name: string, monthlyCost: number) {
+    await postSubject({ name, monthlyCost });
+    await reloadSubjects();
+  }
+
+  async function deleteSubject(id: string) {
+    await deleteSubjectApi(id);
+    await reloadSubjects();
+  }
+
+  useEffect(() => {
+    reloadSubjects();
+  }, []);
+
+  return (
+    <SubjectsContext.Provider
+      value={{ subjects, reloadSubjects, addSubject, deleteSubject }}
+    >
+      {children}
+    </SubjectsContext.Provider>
+  );
+}
+
+export function useSubjects() {
+  const ctx = useContext(SubjectsContext);
+  if (!ctx) throw new Error("useSubjects must be inside SubjectsProvider");
+  return ctx;
+}
