22abf78b67f6e72743ee6944ed825d14748fa1c4
Mauro Correa <mauro.correa@xmartlabs.com>
2025-12-30 12:00:19 -0300

    fix: error.tsx not working, fixed

--- my-next-app/app/error.tsx
+++ my-next-app/app/error.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-export default function Error({ reset }: { reset: () => void }) {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({ reset }: { reset: () => void }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div className="p-10 text-center space-y-4">
       <p className="text-red-600">Something went wrong loading this page.</p>
-      <button onClick={reset} className="px-4 py-2 bg-black text-white rounded">
+      <button
+        onClick={reload}
+        className="px-4 py-2 bg-black text-white rounded"
+      >
         Try again
       </button>
     </div>
