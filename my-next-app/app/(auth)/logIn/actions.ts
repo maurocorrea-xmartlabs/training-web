ada90dd5effefa23fe243e2266f2bc61114ad982
Mauro Correa <mauro.correa@xmartlabs.com>
2026-01-07 11:14:46 -0300

    fix: explaining comments, removing generated folder and reducing signup cooldown

--- my-next-app/app/(auth)/logIn/actions.ts
+++ my-next-app/app/(auth)/logIn/actions.ts
@@ -4,6 +4,7 @@ import { UserLogIn } from "@/types/user";
 import { revalidatePath } from "next/cache";
 
 export async function logInAction(data: UserLogIn) {
+  // The logIn feature is not implemented yet, so I comment this to avoid exceptions
   //await logIn(data);
   revalidatePath("/logIn");
   return true;
