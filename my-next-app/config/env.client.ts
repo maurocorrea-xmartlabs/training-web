4ead44b5dcbc4d4068176235168b7acba1bb88b9
Mauro Correa <mauro.correa@xmartlabs.com>
2026-01-21 09:02:08 -0300

    fix: urls

--- my-next-app/config/env.client.ts
+++ my-next-app/config/env.client.ts
@@ -22,5 +22,5 @@ export function getAppUrl() {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  return "http://localhost:3000";
+  return process.env.NEXT_PUBLIC_APP_URL;
 }
--- my-next-app/config/env.server.ts
+++ my-next-app/config/env.server.ts
@@ -37,5 +37,5 @@ export function getAppUrl() {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  return "http://localhost:3000";
+  return process.env.NEXT_PUBLIC_APP_URL;
 }
